418536d72662ef8491d01a2a0b191105d77960d0
kandyba <m.kandyba@comparus.de>
2023-06-09 18:08:59 +0200

    draft of database

---
+++ database.tf
@@ -0,0 +1,43 @@
+resource "fly_app" "databaseApp" {
+  name = "kandyba-mysql"
+  org  = "personal"
+}
+
+resource "fly_volume" "databaseVolume" {
+  name   = "kandyba_mysql_volume"
+  app    = "kandyba-mysql"
+  size   = 1
+  region = var.fly_region
+}
+
+resource "null_resource" "unmount_db_volume_in_bg" {
+  depends_on = [fly_volume.databaseVolume]
+
+  provisioner "local-exec" {
+    when    = destroy
+    command = "sleep 60"
+  }
+}
+
+resource "fly_machine" "db" {
+  app        = "kandyba-mysql"
+  region     = var.fly_region
+  name       = "kandyba-mysql-machine"
+  image      = "mysql:5.7.28"
+  cpus       = 1
+  memorymb   = 256
+  depends_on = [null_resource.unmount_db_volume_in_bg]
+  mounts = [
+    {
+      path   = "/data"
+      volume = fly_volume.databaseVolume.id
+    }
+  ]
+  env = {
+    MYSQL_DATABASE      = "some_db"
+    MYSQL_USER          = "admin"
+    MYSQL_PASSWORD      = "notasecurepassword"
+    MYSQL_ROOT_PASSWORD = "thesame"
+  }
+  cmd = ["--datadir /data/mysql"]
+}
--- outputs.tf
+++ outputs.tf
@@ -13,3 +13,19 @@ output "memorymb" {
 output "volume_size_gb" {
   value = fly_volume.exampleVolume.size
 }
+
+output "db_cpus" {
+  value = fly_machine.db.cpus
+}
+
+output "db_cputype" {
+  value = fly_machine.db.cputype
+}
+
+output "db_memorymb" {
+  value = fly_machine.db.memorymb
+}
+
+output "db_volume_size_gb" {
+  value = fly_volume.databaseVolume.size
+}
